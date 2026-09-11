35d8c81b72e32daf2302e480e2da73a4fd4e6f9e
Myles Holt <myles.holt.12@gmail.com>
2026-09-11 00:54:05 -0400

    -add v3 despawn animation for targets

--- src/pages/GameBoard.tsx
+++ src/pages/GameBoard.tsx
@@ -264,7 +264,7 @@ export default function GameBoard({mode}: GameBoardProps) {
                 {visibleTargets.map((targ, idx) => (
                     <Fragment key={`${targ.left}-${targ.top}-${targ.spawnTime}`}>
                         <div
-                            className={`${mode === "v3" ? styles.targetV3 : styles.target} ${mode === "v1" ? styles[`step${idx}`] : ''}`}
+                            className={`${mode === "v3" ? styles.targetV3 : styles.target} ${mode === "v3" && targ.hitTime !== undefined && timer > targ.hitTime ? styles.aboutToDespawn : '' } ${mode === "v1" ? styles[`step${idx}`] : ''}`}
                             style={{ left: targ.left + '%', top: targ.top + '%' }}
                             onMouseDown={() => clickTarget(idx)}
                             // onTransitionEnd={exiting && idx === 0 ? (e) => handleFadeEnd(targ, e) : undefined}
